17146471cc6ff81816b53883634be5212bc2bff9
Anna Ivanova <annaivano5a@gmail.com>
2026-02-25 19:27:02 +0200

    done

--- bonus.py
+++ bonus.py
@@ -1,3 +1,4 @@
+#get salary, has to be positive, more than 0, numeric (can be float)
 while True:
     try:
         sal=float(input("Input your monthly salary: "))
@@ -7,16 +8,20 @@ while True:
             break
     except ValueError:
         print("Please enter a valid number for salary.")
-
+#get years, has to be positive, can be 0 (if worked less than a year), numeric (int because full years only)
 while True:
     try:
         yrs=int(input("Input your years of service: "))
-        break 
+        if yrs < 0:
+            print("Years of service cannot be negative.")
+        else:
+            break
     except ValueError:
         print("Please enter a valid integer for years of service.")
-
+# if service over 2 years, for every year 15% bonus
 if yrs > 2:
-    bonus = (sal * 0.15) * (yrs-2)
+    yrs_overtwo = yrs - 2
+    bonus = (sal * 0.15) * yrs_overtwo
     print(f"Your bonus is: {bonus:.2f}")
 else:    
     print("Unfortunately, you are not eligible for a bonus yet.")
